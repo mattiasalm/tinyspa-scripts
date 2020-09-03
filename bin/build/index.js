fbc3eb792332886779669e6db67ae39d6d373a6e
Mattias Alm <mattiasalm@gmail.com>
2020-09-03 17:11:55 +0200

    Add some more

--- bin/build/index.js
+++ bin/build/index.js
@@ -1,9 +1,19 @@
+const { exec } = require('child_process');
+const {series} = require('async');
+
 const createManifest = require('./create-manifest');
 const createIcons = require('./create-icons');
 const createRobots = require('./create-robots');
 const createBasicServiceWorker = require('./create-basic-service-worker');
 
-createManifest();
-createIcons();
-createRobots();
-createBasicServiceWorker();
+const create = () => {
+  createManifest();
+  createIcons();
+  createRobots();
+  createBasicServiceWorker();
+};
+
+exec('./node_modules/.bin/rollup -c', (err, stdout, stderr) => {
+  console.log(stdout);
+  console.log(stderr);
+});
